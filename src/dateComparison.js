4c65e75ca79d808d3dbd0e5e8f1431748a67f22f
Kim Joohwan <kim.joohwan58@gmail.com>
2023-07-19 15:08:18 +0900

    testted dateComparison module. todo: implement dateComparison into app.js

--- src/dateComparison.js
+++ src/dateComparison.js
@@ -1,17 +1,51 @@
 import { compareAsc } from "date-fns";
+import add from 'date-fns/add'
 
 const checkDueDate = (() => {
-    let currentDate = new Date();
+    let date = new Date();
+    let currentDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    let weekBoundary = add(currentDate, {
+        years: 0,
+        months: 0,
+        weeks: 1,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    })
 
-    const duePast = (todoDate) => {
-        if (compareAsc(currentDate, todoDate)) {
-            console.log('not past');
+    const overdue = (todoDate) => {
+        if (compareAsc(currentDate, todoDate) == 1) {
+            return true;
         } else {
-            console.log('past');
+            return false;
         }
     }
 
-    return { duePast }
+    const dueToday = (todoDate) => {
+        if (overdue(todoDate)) {
+            return false;
+        } else {
+            if (compareAsc(currentDate, todoDate) == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+    const dueThisWeek = (todoDate) => {
+        let array = [currentDate, todoDate, weekBoundary];
+        array.sort(compareAsc);
+        if (array[1] == todoDate) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return { currentDate, overdue, dueToday, dueThisWeek }
 })();
 
 export { checkDueDate }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { projectFactory } from './project.js'
 import { app } from './app.js';
 import { checkDueDate } from './dateComparison.js';
 
-let testDate = new Date(2023, 7, 10);
+let testDate = new Date(2023, 6, 27);
 
+console.log(checkDueDate.currentDate);
 
-
+console.log(checkDueDate.overdue(testDate));
+console.log(checkDueDate.dueToday(testDate));
+console.log(checkDueDate.dueThisWeek(testDate));
